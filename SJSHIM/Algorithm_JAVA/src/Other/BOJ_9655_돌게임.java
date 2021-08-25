e5aa147fcee0d0e8dcd108a16b54d828be7e156a
ShimSeongJin <33081162+shimseongjin@users.noreply.github.com>
2021-08-26 01:15:16 +0900

    [ADD] BOJ_9655_돌게임
    규칙성을 찾아 풀이한 문제

---
+++ SJSHIM/Algorithm_JAVA/src/Other/BOJ_9655_돌게임.java
@@ -0,0 +1,19 @@
+package Other;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ_9655_돌게임 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		if(N % 2 == 0) {
+			System.out.println("CY");
+		}else {
+			System.out.println("SK");
+		}
+	}
+
+}
